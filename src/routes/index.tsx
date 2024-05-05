f3221f1b025cfadf5207ef4ced3bd1fdeb35ce1b
amotahar <motaharhosssain.cse@gmail.com>
2024-05-05 23:51:57 +0600

    redux toolkit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";import App from "../App";
 import Home from "../page/Home";
 import Login from "../page/Login";
+import NotFound from "../NotFound/NotFound";
 
 const routes = createBrowserRouter([
     {
         path: "/",
         element: <App />
+    },
+    {
+        path: "*",
+        element: <NotFound/>
     },
     {
         path: "/home",
